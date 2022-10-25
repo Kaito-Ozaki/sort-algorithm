2d7a05012098a39dc54673d8f52cb3901050ceae
尾崎海斗 <okaito@ozakikaitonoMacBook-Air.local>
2022-10-25 22:56:38 +0900

    細かいリファクタリング

--- bubble.py
+++ bubble.py
@@ -1,6 +1,7 @@
 def bubble_sort(li):
-    for i in range(len(li)):
-        for j in range(len(li)-1):
+    n = len(li)
+    for i in range(n):
+        for j in range(n-1):
             if li[j] >= li[j+1]:
                 li[j], li[j+1] = li[j+1], li[j]
     return li
--- selection.py
+++ selection.py
@@ -1,7 +1,8 @@
 def selection_sort(li):
-    for i in range(0, len(li)-1):
+    n = len(li)
+    for i in range(0, n-1):
         min = i
-        for j in range(i+1, len(li)):
+        for j in range(i+1, n):
             if li[j] < li[min]:
                 min = j
         li[i], li[min] = li[min], li[i] 
