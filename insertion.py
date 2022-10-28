0246f9110f2b459823c252c78d8c723d0a5df547
尾崎海斗 <okaito@ozakikaitonoMacBook-Air.local>
2022-10-28 13:42:40 +0900

    挿入ソートのロジックを修正

--- insertion.py
+++ insertion.py
@@ -1,8 +1,10 @@
 def insertion_sort(li):
     for i in range(1, len(li)):
-        ins = i
-        for j in range(0, i+1):
-            if li[i] >= li[j]:
-                ins = j
-        li[i], li[ins] = li[ins], li[i]
+        tmp = li[i]
+        j = i - 1
+
+        while j >= 0 and li[j] > tmp:
+            li[j+1] = li[j]
+            j = j - 1
+        li[j+1] = tmp
     return li
